7c570d569ecd465e2bb83d9cb327d5e7c5e95233
Pascal Bertschi <pascal@bertschi.io>
2021-03-21 19:08:51 +0100

    fix url for rpc client

--- packages/hsr-browser-rpc/rpc-browser-client.ts
+++ packages/hsr-browser-rpc/rpc-browser-client.ts
@@ -3,11 +3,11 @@ import { browserClient } from '../hsr-browser/browser-client'
 import { RpcClient } from './rpc-client'
 
 export function rpcBrowserClient<T extends RpcServerInterface<any, any>>(url?: string): RpcClient<T> {
-  const client = browserClient()
+  const client = browserClient(url)
   return {
     async call<TCall extends T['_calls'], A extends T['_decorders'], P extends string & keyof TCall>(
       name: P,
-      arg: A,
+      arg: A
     ): Promise<T['_calls'][P]> {
       const result = await client.body(JSON.stringify(arg)).post(`/api/rpc/${name}`)
       return result.bodyAsJson()
