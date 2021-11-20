4d63b92e7c816290d0f19f10f4d720d9468fb71e
Pascal Bertschi <pascal@bertschi.io>
2021-11-20 13:50:00 +0100

    add error handler

--- packages/hsr-node-rpc/src/build-http-plugin.ts
+++ packages/hsr-node-rpc/src/build-http-plugin.ts
@@ -1,7 +1,15 @@
 import { RpcServer } from './rpc-server'
 import { HttpPlugin } from '@no0dles/hsr-node/server/http-plugin'
+import { HttpResponse } from '@no0dles/hsr-node/server/http-response'
 
-export function buildHttpPlugin(rpcServer: RpcServer<any, any>): HttpPlugin<any> {
+export interface HttpRpcOptions {
+  errorHandler: HttpRpcErrorHandler
+}
+
+export type HttpRpcErrorHandler = (error: Error, res: HttpResponse) => HttpResponse
+
+export function buildHttpPlugin(rpcServer: RpcServer<any, any>, options?: Partial<HttpRpcOptions>): HttpPlugin<any> {
+  const errorHandler: HttpRpcErrorHandler = options?.errorHandler ?? ((e, res) => res.statusCode(500))
   return (router) => {
     for (const key of Object.keys(rpcServer._calls)) {
       router.path(`/api/rpc/${key}`).post(async (req, res) => {
@@ -10,8 +18,11 @@ export function buildHttpPlugin(rpcServer: RpcServer<any, any>): HttpPlugin<any>
           const result = await rpcServer.execute(key, data)
           return res.statusCode(200).json(result)
         } catch (e) {
-          console.error(e)
-          return res.statusCode(500)
+          try {
+            return errorHandler(e, res)
+          } catch (e) {
+            return res.statusCode(500)
+          }
         }
       })
     }
